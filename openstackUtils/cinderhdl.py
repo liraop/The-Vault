3bf6d61fdaec06dd17ed191fb5d83753688e2c9c
root <lirapdo@gmail.com>
2015-10-29 10:49:02 -0300

    cinderhdl fixed. changing states from error to in-use. TODO: restart cinder service in storage01/ncc1701-d after change volume's status.

--- openstackUtils/cinderhdl.py
+++ openstackUtils/cinderhdl.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import cinderclient.v1 as cclient
-from credentials import get_nova_creds
+import cinderclient.v1.volumes as volclient
+import os
 
-credentials = get_nova_creds
-cinder = cclient.Client(credentials)
+cinder = cclient.Client(os.environ['OS_USERNAME'],os.environ['OS_PASSWORD'],os.environ['OS_TENANT_NAME'],os.environ['OS_AUTH_URL'],service_type="volume")
+volmgmt = volclient.VolumeManager(cinder)
+
+for volume in  cinder.volumes.list(search_opts={'all_tenants': 1}):
+	if volume.status == 'error':
+		volmgmt.reset_state(volume.id,'in-use')
--- openstackUtils/credentials.py
+++ openstackUtils/credentials.py
@@ -17,3 +17,12 @@ def get_nova_creds():
     d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
 
+def get_cinder_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    d['service_type'] = "volume" 
+    return d
+
