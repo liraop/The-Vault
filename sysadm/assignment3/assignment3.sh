374b219bbdc56627477bacdcb3c4c4edfbf21dae
liraop <lirapdo@gmail.com>
2016-07-29 00:28:48 -0300

    assignment3: finished. TO DO: better diff; modularization; extra feature;

--- sysadm/assignment3/assignment3.sh
+++ sysadm/assignment3/assignment3.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+        ASSIGNSET=$(ls *.sh)
+	
+	for assign in $ASSIGNSET
+	do
+		TESTPATTERN=$(echo $assign | cut -d"." -f1 | cut -d"_" -f1-2)
+		TESTSET=$(ls *.in | grep $TESTPATTERN)
+                
+		for test in $TESTSET
+                do
+			TESTID=$(echo $test | cut -d'_' -f3 | cut -d'.' -f1)
+                        echo "-SAIDA PARA ENTRADA $TESTID:"
+
+                        TESTRESULT=$(sh $assign `cat $test`)
+                        echo "$TESTRESULT"
+
+                        OUTFILE=$(echo $test | cut -d"." -f1)
+                        echo $TESTRESULT > $OUTFILE.out
+
+                        echo "DIFERENCA PARA A SAIDA ESPERADA:"
+                        DIFF=$(diff "$test" "$OUTFILE.out")
+                        echo $DIFF
+		done
+        done
+fi
+
+
 if [ "$#" -eq 1 ]; then
 	EXER="EXERCICIO_$1"
 
@@ -12,7 +39,7 @@ if [ "$#" -eq 1 ]; then
 		echo "$ASSIGNID"
 		for test in $TESTSET
 		do
-			TESTID=$(echo $test | cut -d'_' -f2)
+			TESTID=$(echo $test | cut -d'_' -f2 | cut -d'.' -f1)
 	                echo "-SAIDA PARA ENTRADA $TESTID:"
 
 	                TESTRESULT=$(sh $assign `cat $test`)
