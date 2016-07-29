2db3ac4a56646c41b57ef3a93626e337d193ef38
liraop <lirapdo@gmail.com>
2016-07-29 14:35:38 -0300

    assignment3: refactor completed. TO DO: extra feature

--- sysadm/assignment3/assignment3.sh
+++ sysadm/assignment3/assignment3.sh
@@ -1,90 +1,37 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
-        ASSIGNSET=$(ls *.sh)
-	
-	for assign in $ASSIGNSET
-	do
-		TESTPATTERN=$(echo $assign | cut -d"." -f1 | cut -d"_" -f1-2)
-		TESTSET=$(ls *.in | grep $TESTPATTERN)
-                
+if [ "$#" -gt 0 ]; then
+        EXER="EXERCICIO_$1"
+        TESTSET=$(ls *.in | grep "$EXER")
+
+	if [ "$#" -eq 2 ]; then
+		ASSIGNSET="$2"
+	else
+	        ASSIGNSET=$(ls *.sh | grep "$EXER")
+	fi
+else 
+        ASSIGNSET=$(ls EXERCICIO*.sh )
+fi	
+
+for assign in $ASSIGNSET
+do
+		TESTPATTERN=$(echo $assign | cut -d"." -f1 | cut -d"_" -f1-2)		# pattern to match tests in folder
+		TESTSET=$(ls *.in | grep $TESTPATTERN) 					# set of tests found in folder
+		ASSIGNID=$(echo $assign | cut -d"." -f1)
+		echo "$ASSIGNID:"
 		for test in $TESTSET
                 do
 			TESTID=$(echo $test | cut -d'_' -f3 | cut -d'.' -f1)
                         echo "-SAIDA PARA ENTRADA $TESTID:"
 
-                        TESTRESULT=$(sh $assign `cat $test`)
-                        echo "$TESTRESULT"
+                        TESTRESULT=$(sh $assign `cat $test`)				# run script with test input
+                        echo -e "$TESTRESULT \n"
 
-                        OUTFILE=$(echo $test | cut -d"." -f1)
+                        OUTFILE=$(echo $test | cut -d"." -f1)				# test output
                         echo $TESTRESULT > $OUTFILE.out
 
                         echo "DIFERENCA PARA A SAIDA ESPERADA:"
-                        DIFF=$(diff "$test" "$OUTFILE.out")
-                        echo $DIFF
-		done
-        done
-fi
-
-
-if [ "$#" -eq 1 ]; then
-	EXER="EXERCICIO_$1"
-
-	TESTSET=$(ls *.in | grep "$EXER")
-	ASSIGNSET=$(ls *.sh | grep "$EXER")
-	
-	for assign in $ASSIGNSET
-	do
-		ASSIGNID=$(echo $assign | cut -d"." -f1)
-		echo "$ASSIGNID"
-		for test in $TESTSET
-		do
-			TESTID=$(echo $test | cut -d'_' -f2 | cut -d'.' -f1)
-	                echo "-SAIDA PARA ENTRADA $TESTID:"
-
-	                TESTRESULT=$(sh $assign `cat $test`)
-	                echo "$TESTRESULT"
-
-	                OUTFILE=$(echo $test | cut -d"." -f1)
-	                echo $TESTRESULT > $OUTFILE.out
-
-	                echo "DIFERENCA PARA A SAIDA ESPERADA:"
-	                DIFF=$(diff "$test" "$OUTFILE.out")
-	                echo $DIFF
-		done	
+                        DIFF=$(diff "$test" "$OUTFILE.out")				# diff from test output vs expected output
+                        echo -e "$DIFF\n"
 	done
-fi
-
-
-if [ "$#" -eq 2 ]; then
-	HW="$2"
-	EXER="$1"
-	
-	### question number to be tested
-	### 
-	TMP=$(echo $HW | cut -d"_" -f1-2)
-
-	### tests for the question in the folder
-	###
-	TESTSET=$(ls *.in | grep $TMP)
-
-	echo "$HW:"
-	
-	for test in $TESTSET
-	do
-		TESTID=$(echo $test | cut -d'_' -f3 | cut -d'.' -f1)
-		echo "-SAIDA PARA ENTRADA $TESTID:"
-
-		TESTRESULT=$(sh $HW `cat $test`)
-		echo "$TESTRESULT"
-
-		OUTFILE=$(echo $test | cut -d"." -f1)
-		echo $TESTRESULT > $OUTFILE.out
-
-		echo "DIFERENCA PARA A SAIDA ESPERADA:"
-		DIFF=$(diff "$test" "$OUTFILE.out")
-		echo $DIFF
-	done	
-fi
-
-
+done
