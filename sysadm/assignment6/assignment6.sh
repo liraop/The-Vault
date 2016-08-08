802f74c9a87401e73c5960f6f9a09fec1031efb2
liraop <lirapdo@gmail.com>
2016-08-08 13:36:34 -0300

    assignment6 validated and finished.

--- sysadm/assignment6/assignment6.sh
+++ sysadm/assignment6/assignment6.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 medianAverage(){
-	local PING=$1
+	# this grep rexp gives a safe margin for timing
+	TIMECOLUMM=$(echo "$1" | grep -o "time=[0-9]\{1,4\}*.[0-9]\{1,4\}" | cut -d"=" -f2 | sort -g) 
 
-	TIMECOLUMM=$(echo "$PING" | grep -o "time=[0-9]\{1,4\}.[0-9]\{3\}" | cut -d"=" -f2 | sort)
-	MEDIAN=$(echo "$TIMECOLUMM" | sed -n 5,6p | awk '{sum += $1} END {print sum/2}')
-	AVERAGE=$(echo "$TIMECOLUMM" | tail -n 3 | awk '{sum += $1} END {print sum/3}')
-	
-	### extra feature - average of faster replies 
+	# since it's an even number of elements, median = (central element 1  + central element 2) / 2
+	MEDIAN=$(echo "$TIMECOLUMM" | sed -n 5,6p | awk '{sum += $1} END {print sum/2}') 
 
-	FASTERAVERAGE=$(echo "$TIMECOLUMM" | head -n 3 | awk '{sum += $1} END {print sum/3}')
+	LONGERAVERAGE=$(echo "$TIMECOLUMM" | tail -n 3 | awk '{sum += $1} END {print sum/3}')
 	
-	echo "Pings max (slower) average time: $AVERAGE ms"
-	echo "Pings median: $MEDIAN ms"
-	echo "Pings min (faster) time: $FASTERAVERAGE ms"
+	### extra feature - average of 3 faster replies 
+	SHORTERAVERAGE=$(echo "$TIMECOLUMM" | head -n 3 | awk '{sum += $1} END {print sum/3}')
+	
+	printf "3 longer requests average time: $LONGERAVERAGE ms
+requests time median: $MEDIAN ms
+3 shorter requests time: $SHORTERAVERAGE ms\n\n"
 }
 
 
 URL="uol.com.br"
 
-echo -n "Pinging default size"
+printf "Pinging default packet size\n\n"
 PING1=$(ping -c 10 $URL)
 medianAverage "$PING1"
 
 sleep 10
 
-echo -n "Pinging packet size = 64"
+printf "Pinging packet size = 64\n\n"
 PING2=$(ping -c 10 -s 64 $URL)
 medianAverage "$PING2"
 
@@ -42,7 +43,7 @@ medianAverage "$PING2"
 ###	D = icmp_seq is an "id" for the request. Each ICMP reply shall match its request, e.g. the
 ###	   first request shall receive the reply with  icmp_seq=1 
 ###	E = TTL (time-to-live) is the "lifespan" of a ping request to reach the destination. At each 
-###	   router, this number is drecremented by one. If it reaches 0, the packet is discarded.
+###	   router, this number is drecremented by one. If it reaches, the packet is discarded.
 ###	F = delay time from sending sending the ping and receiving the response
 
 ### 3)	X is a domain name, it can be represented by a farm/cluster of hosts as a measure to improve 
