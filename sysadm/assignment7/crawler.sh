1f8fd1762f4f5e1dabd989ba7ddf19a2e043e567
liraop <lirapdo@gmail.com>
2016-08-14 16:35:32 -0300

    assignment7 partially finished; TODO: save page to file; fix features for two arguments

--- sysadm/assignment7/crawler.sh
+++ sysadm/assignment7/crawler.sh
@@ -2,17 +2,30 @@
 
 SITES=`cat $1`
 
+if [ $# -gt 1  ]; then
+	WORDS=`cat $2`
+else 
+	WORDS=""
+fi
 
-for site in $SITES;
+WORDSTATUS="OK!"
+
+for url in $SITES;
 do
-	STATUS=$(curl -s --head $site | head -n 1 | cut -d" " -f2)
-	if [ $STATUS != "400" ]; then
-		SITE=`wget -q $site`
-		for palavra in `cat $2`;
-		do 
-			RESULTT=$(echo $SITE | grep $palavra)
-		done
-		if [[ -z $RESULT ]]; then
-	fi
-	printf "$site $STATUS \n"
+	COUNTER=0
+	TOTAL=0
+	STATUS=$(curl --silent --head $url | head -n 1 | cut -d" " -f2)	# site status
+	SITE=`wget -qO- $url`						# download page and save it on variable
+	for word in $WORDS;
+	do
+		RESULT=$(echo $SITE | grep $word)
+	        if [[ -z $RESULT ]]; then				# if the word is not found
+			WORDSTATUS=""					# it returns blank
+		else 
+			COUNTER=$((COUNTER+1))
+		fi
+		TOTAL=$((TOTAL+1))
+	done
+	printf "$url $STATUS $WORDSTATUS\n"
+	printf "Words found: $COUNTER out of $TOTAL \n"			# extra feature - show # of found words
 done
