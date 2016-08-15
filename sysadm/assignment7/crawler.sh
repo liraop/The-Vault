399996a089908cb55a9daf0725407355bad0f2e5
liraop <lirapdo@gmail.com>
2016-08-15 10:37:40 -0300

    assignment7 finished

--- sysadm/assignment7/crawler.sh
+++ sysadm/assignment7/crawler.sh
@@ -3,29 +3,31 @@
 SITES=`cat $1`
 
 if [ $# -gt 1  ]; then
-	WORDS=`cat $2`
-else 
-	WORDS=""
+        WORDS=`cat $2`
+	WORDSTATUS="OK!"
+#else
+#        WORDS=""
+#	MSG=""
 fi
 
-WORDSTATUS="OK!"
 
 for url in $SITES;
 do
-	COUNTER=0
-	TOTAL=0
-	STATUS=$(curl --silent --head $url | head -n 1 | cut -d" " -f2)	# site status
-	SITE=`wget -qO- $url`						# download page and save it on variable
-	for word in $WORDS;
-	do
-		RESULT=$(echo $SITE | grep $word)
-	        if [[ -z $RESULT ]]; then				# if the word is not found
-			WORDSTATUS=""					# it returns blank
-		else 
-			COUNTER=$((COUNTER+1))
-		fi
-		TOTAL=$((TOTAL+1))
-	done
-	printf "$url $STATUS $WORDSTATUS\n"
-	printf "Words found: $COUNTER out of $TOTAL \n"			# extra feature - show # of found words
+        COUNTER=0
+        TOTAL=0
+        STATUS=$(curl --silent --head $url | head -n 1 | cut -d" " -f2) # site status
+        SITE=`wget -qO- $url`                                           # download page and save it on variable
+        for word in $WORDS;
+        do
+                RESULT=$(echo $SITE | grep $word)
+                if [ -z "$RESULT" ]; then                               # if the word is not found
+                        WORDSTATUS=""                                   # it returns blank
+                else
+                        COUNTER=$((COUNTER+1))
+                fi
+                TOTAL=$((TOTAL+1))
+		MSG="Words found: $COUNTER out of $TOTAL \n"
+        done
+        printf "$url $STATUS $WORDSTATUS\n"
+        printf "$MSG"					                # extra feature - show found words #
 done
