de2158188a44e77ba70e7dff06ab3a556996d506
liraop <lirapdo@gmail.com>
2016-08-26 17:11:06 -0300

    assignment9: extra feature left

--- sysadm/assignment9/asg9.sh
+++ sysadm/assignment9/asg9.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function convertToSeconds()
+{
+	echo $1 | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'
+}
+
 URL=$1
 GETTIME="grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
 GETRTT="grep -o 'RTT:[0-9].[0-9]\{1,3\}' | cut -d: -f2 "
@@ -12,5 +17,10 @@ printf "$RTT\n"
 SERVERTIME=`echo $CURLGET | eval $GETTIME`
 printf "$SERVERTIME\n"
 
-LOCALTIME=`uptime | eval $GETTIME`
+LOCALTIME=`date | eval $GETTIME`
 printf "$LOCALTIME\n"
+
+LTS=`convertToSeconds $LOCALTIME`
+STS=`convertToSeconds $SERVERTIME`
+
+echo "$LTS $STS" | awk -F'-' '{print $1-$2}'
