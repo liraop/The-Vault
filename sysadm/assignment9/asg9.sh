76f0868c22cabca14f3bb8a32740d0d1f7be9292
liraop <lirapdo@gmail.com>
2016-08-26 16:46:16 -0300

    assignment9: RTT display OK

--- sysadm/assignment9/asg9.sh
+++ sysadm/assignment9/asg9.sh
@@ -2,13 +2,12 @@
 
 URL=$1
 GETTIME="grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
-GETRTT="grep 'RTT'"
+GETRTT="grep -o 'RTT:[0-9].[0-9]\{1,3\}' | cut -d: -f2 "
 
 CURLGET=`curl --silent -Iw RTT:%{time_total} $URL`
 
-#RTT=`echo $CURLGET | eval $GETRTT `
-#printf "$RTT\n
-
+RTT=`echo $CURLGET | eval $GETRTT `
+printf "$RTT\n"
 
 SERVERTIME=`echo $CURLGET | eval $GETTIME`
 printf "$SERVERTIME\n"
