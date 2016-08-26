aef2ede86f72db13bbe1dc774b898f4cec1be2ee
liraop <lirapdo@gmail.com>
2016-08-26 16:13:03 -0300

    assignment9: local and server time OK

--- sysadm/assignment9/asg9.sh
+++ sysadm/assignment9/asg9.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+URL=$1
+GETTIME="grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
+GETRTT="grep 'RTT'"
 
-curl -vsI www.google.com | grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]"
+CURLGET=`curl --silent -Iw RTT:%{time_total} $URL`
+
+#RTT=`echo $CURLGET | eval $GETRTT `
+#printf "$RTT\n
+
+
+SERVERTIME=`echo $CURLGET | eval $GETTIME`
+printf "$SERVERTIME\n"
+
+LOCALTIME=`uptime | eval $GETTIME`
+printf "$LOCALTIME\n"
