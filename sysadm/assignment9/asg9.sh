a72035d7c596a64ed3c66825d4c4c289f0ae0499
liraop <lirapdo@gmail.com>
2016-08-27 13:28:16 -0300

    extra feature added

--- sysadm/assignment9/asg9.sh
+++ sysadm/assignment9/asg9.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+
+### 
+### funciontion to convert HH:MM:SS into seconds
+###
 function convertToSeconds()
 {
 	echo $1 | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'
 }
 
 URL=$1
-GETTIME="grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
-GETRTT="grep -o 'RTT:[0-9].[0-9]\{1,3\}' | cut -d: -f2 "
+GETTIME="grep -oh "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""			### pattern to grep time
+GETRTT="grep -o 'RTT:[0-9].[0-9]\{1,3\}' | cut -d: -f2 "		### rtt(operation total time) pattern
 
-CURLGET=`curl --silent -Iw RTT:%{time_total} $URL`
+CURLGET=`curl --silent -Iw RTT:%{time_total} $URL` 			### curl with http header and total operation time in seconds
+
+### extra feature
+### check if domain exists
+if [ $? -gt 0 ]; then
+	printf "Domain is offline or does not exist. Try another one\n"
+	exit 1
+fi
 
 RTT=`echo $CURLGET | eval $GETRTT `
-printf "$RTT\n"
+printf "$RTT\n"								### curl get operation total time in seconds
 
 SERVERTIME=`echo $CURLGET | eval $GETTIME`
-printf "$SERVERTIME\n"
+printf "$SERVERTIME\n"							### http header server time
 
 LOCALTIME=`date | eval $GETTIME`
-printf "$LOCALTIME\n"
+printf "$LOCALTIME\n"							### current localtime
 
 LTS=`convertToSeconds $LOCALTIME`
 STS=`convertToSeconds $SERVERTIME`
 
-echo "$LTS $STS" | awk -F'-' '{print $1-$2}'
+echo "$LTS $STS" | awk -F'-' '{print $1-$2}'				### local time - server time in seconds
+exit 0
