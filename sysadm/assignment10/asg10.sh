2719f6f39d4733a29da88f5dc5f09bc52027ea97
liraop <lirapdo@gmail.com>
2016-08-29 21:08:27 -0300

    assignment10: function to test if file exists

--- sysadm/assignment10/asg10.sh
+++ sysadm/assignment10/asg10.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+###
+### check if file is exists
+###
+function checkFile()
+{
+	if [[ `ls | grep -wc "$1"` -gt 0 ]]; then
+		echo 1					### found
+	else 
+		echo 0					### not found 
+	fi
+}
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	CODE=$(echo "$line" | cut -d" " -f1)
 	METHOD=$(echo "$line" | cut -d" " -f2)
 	FILE=$(echo "$line" | cut -d" " -f3)
+	
 done < "$1"
