7a8379ad6e43e3f901dc6b80218876c2425c82c0
liraop <lirapdo@gmail.com>
2016-08-29 23:48:27 -0300

    assignment10: extra feature added; finished

--- sysadm/assignment10/asg10.sh
+++ sysadm/assignment10/asg10.sh
@@ -34,6 +34,41 @@ function checkCode()
 	fi
 }
 
+###
+### extra feature - generate code for files in directory
+###
+
+function genCodes()
+{
+	FILE=$1
+	RESULT="File name: $FILE\n"
+	TMP=`md5sum $FILE | cut -d" " -f1`
+	RESULT="${RESULT}MD5: $TMP\n"
+        TMP=`sha1sum $FILE | cut -d" " -f1`
+	RESULT="${RESULT}SHA1: $TMP\n"
+	TMP=`cksum $FILE | cut -d" " -f1`
+	RESULT="${RESULT}CRC: $TMP\n"
+
+	printf '%b\n' "$RESULT"
+}
+
+###
+### extra feature - when 0 inputs
+###
+
+if [[ $# -eq 0 ]]; then
+	LOCALDIR=`ls -p | grep -v /`
+	for FILE in $LOCALDIR
+	do
+		genCodes $FILE 
+	done
+	exit 0
+fi
+
+###
+### end extra feature
+###
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	CODE=$(echo "$line" | cut -d" " -f1)
 	METHOD=$(echo "$line" | cut -d" " -f2)
