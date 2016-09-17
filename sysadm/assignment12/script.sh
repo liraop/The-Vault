bbf58ff37cf502db881deeb1e5104e753b8462b4
liraop <lirapdo@gmail.com>
2016-09-17 19:27:46 -0300

    assignment12: functions: checkVersion; getFinalName

--- sysadm/assignment12/script.sh
+++ sysadm/assignment12/script.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+###
+### remove bars from paths 
+###
+function getFinalName()
+{
+	ABSOLUTE=$1
+	echo $1 | rev | cut -d"/" -f1 | rev
+}
+
+###
+### function to copy $1 to $2
+###
 function createCopy()
 {
 	SOURCE=$1
@@ -7,15 +19,27 @@ function createCopy()
 	rsync -ar $1 $2
 }
 
-if [ "$#" -eq 2 ];
-then
-
-elif [ "$#" -eq 3 ];
-then
-	if [ "$1" == "-z" ];
-	then
-
-	else
+###
+### check files' version in
+### the target folder. 
+###
+function checkVersion()
+{
+	FILE=$1
+	TARGET=$2
+	GREP=`ls $2 | grep $1`
+	
+	if [[ -z "$GREP" ]]; then
+		printf "NADA"
 	fi
+}
 
+if [ "$#" -eq 2 ]; then
+	checkVersion $1 $2
+elif [ "$#" -eq 3 ]; then
+	if [ "$1" == "-z" ]; then
+		echo 0
+	else
+		echo 1
+	fi
 fi
