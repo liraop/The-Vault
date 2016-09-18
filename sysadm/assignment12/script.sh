be05f67ac9a090209c1e81f6b02ec906597806d2
liraop <lirapdo@gmail.com>
2016-09-17 21:10:52 -0300

    assignment12: fix target path bug

--- sysadm/assignment12/script.sh
+++ sysadm/assignment12/script.sh
@@ -1,49 +1,83 @@
 #!/bin/bash
 
 ###
+### got from:
+### http://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+###
+function isNumber()
+{
+	 [[ ${1} == *[[:digit:]]* ]]
+}
+
+###
 ### returns the last field of a text
 ### $1 = delimiter
 ### $2 = text
 ###
-function getLastDelimitedField()
+function getLastDelimField()
 {
-	ABSOLUTE=$2
+	TEXT=$2
 	echo $2 | rev | cut -d"$1" -f1 | rev
 }
 
 ###
 ### function to copy $1 to $2
+### $3 = filename
+### $4 = id 
 ###
 function createCopy()
 {
 	SOURCE=$1
 	TARGET=$2
-	rsync -ar $1 $2
+	FILENAME=$3
+	ID=$4
+	FINALTARGET="$TARGET/$FILENAME$ID"
+
+	printf "Criando backup de $SOURCE em $FINALTARGET\n"
+	rsync -ar $SOURCE $FINALTARGET
 }
 
 ###
-### check files' occurrences in
+### check item occurrences in
 ### the target folder. 
+### $1 = item to be counted
+### $2 = target folder
 ###
 function checkOccurrences()
 {
-	FILE=$(getLastDelimitedField / $1)
+	FILE=$1
 	TARGET=$2
-	GREP=`ls $2 | grep $FILE`
+	GREP=`ls $2 | grep ^$FILE$`
 	
 	if [[ -z "$GREP" ]]; then
-		echo 0			## no occurrences on folder
+		echo -1					## no occurrences on folder
 	else
-		echo "$GREP" | rev | cut -d"." -f1 | rev 
+		TMP=$(getLastDelimField "." "$GREP")
+		if [[ $(isNumber $TMP) ]]; then
+			echo $TMP			## number of occurrences
+		else 
+			echo 0				## only one occurency
+		fi
 	fi
 }
 
+SOURCEPATH=$1
+TARGETPATH=$2
+FILENAME=$(getLastDelimField "/" $1)
+
 if [ "$#" -eq 2 ]; then
-	checkOccurrences $1 $2
+	OCCURRENCES=$(checkOccurrences $FILENAME $TARGETPATH)
+	if [[ $OCCURRENCES -ge 0 ]]; then
+		SUFIX=".${OCCURRENCES+1}"
+		createCopy $SOURCEPATH $TARGETPATH $FILENAME $SUFIX
+	else 
+		createCopy $SOURCEPATH $TARGETPATH $FILENAME
+	fi
 elif [ "$#" -eq 3 ]; then
 	if [ "$1" == "-z" ]; then
 		echo 0
 	else
-		echo 1
+		printf "Invalid option"
+		exit 1
 	fi
 fi
