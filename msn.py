f2e5ed499a2f3aabc88884cd28586accaddc0617
liraop <lirapdo@gmail.com>
2017-07-03 15:18:20 -0300

    fix values and prints

--- msn.py
+++ msn.py
@@ -17,13 +17,13 @@ def getTotalProb(K, flux, W0):
 
 def getStateProb(K, flux, W0):
     probSum = getTotalProb(K, flux, W0)
-    print "Total probability for K="+repr(K)+" = "+repr(probSum)
     for i in range(K+1):
-        print "Probability of K="+repr(i)+" = " +\
+        print "Probability of k="+repr(i)+" = " +\
               repr(np.divide(getStateValue(i, flux, W0), probSum))
+    print "Total probability for K="+repr(K)+" = "+repr(probSum)
 
 
-lamb, micro, w0 = 5.0, 8.0, 1
+lamb, micro, w0 = 15.0, 20.0, 1
 flux = np.float64(np.divide(lamb, micro))
 getStateProb(2, flux, w0)
 getStateProb(5, flux, w0)
