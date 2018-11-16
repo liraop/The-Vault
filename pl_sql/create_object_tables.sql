22ec41e2f0145da1070d0751c0ecb34e7f24829a
liraop <pedrolira@live.com>
2018-11-16 10:48:37 -0300

    Removendo constraints para outro arquivo

--- pl_sql/create_object_tables.sql
+++ pl_sql/create_object_tables.sql
@@ -1,22 +1,9 @@
-CREATE OR REPLACE TABLE dim_obj_tab_sala OF sala_type (
-  CONSTRAINT pk_sala PRIMARY KEY (id_sala)
-);
+CREATE TABLE  dim_obj_tab_sala OF sala_type;
 
-CREATE OR REPLACE TABLE dim_obj_tab_professor OF professor_type (
-  CONSTRAINT pk_professor PRIMARY KEY (id_professor)
-);
+CREATE TABLE dim_obj_tab_professor OF professor_type;
 
-CREATE OR REPLACE TABLE dim_obj_tab_disciplina OF disciplina_type (
-  CONSTRAINT pk_disciplina PRIMARY KEY (id_disciplina)
-);
+CREATE TABLE dim_obj_tab_disciplina OF disciplina_type;
 
-CREATE OR REPLACE TABLE dim_obj_tab_curso OF curso_type (
-  CONSTRAINT pk_curso PRIMARY KEY (id_curso)
-);
+CREATE TABLE dim_obj_tab_curso OF curso_type;
 
-CREATE OR REPLACE TABLE fat_obj_tab_alocacao OF turma_type (
-  CONSTRAINT tab_disciplina_fk FOREIGN KEY (id_disciplina) REFERENCES dim_obj_tab_disciplina,
-  CONSTRAINT tab_professor_fk FOREIGN KEY (id_professor) REFERENCES dim_obj_tab_professor,
-  CONSTRAINT tab_curso_fk FOREIGN KEY (id_curso) REFERENCES dim_obj_tab_curso,
-  CONSTRAINT tab_sala_fk FOREIGN KEY (id_sala) REFERENCES dim_obj_tab_sala
-);
+CREATE  TABLE fat_obj_tab_alocacao OF turma_type;
