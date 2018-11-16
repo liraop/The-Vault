0307dfe4f482463b8cce83c58818c52a59a9cd8a
liraop <pedrolira@live.com>
2018-11-16 10:48:07 -0300

    Atualizando parametros de objetos

--- pl_sql/create_object_types.sql
+++ pl_sql/create_object_types.sql
@@ -1,19 +1,17 @@
---2
-
 CREATE OR REPLACE TYPE professor_type AS OBJECT (
   id_professor INTEGER,
-  nome VARCHAR2(20)
+  nome VARCHAR2(15)
 );
 
 CREATE OR REPLACE TYPE sala_type AS OBJECT (
   id_sala INTEGER,
-  codigo VARCHAR2(10),
+  codigo VARCHAR2(8),
   capacidade INTEGER
 );
 
 CREATE OR REPLACE TYPE disciplina_type AS OBJECT (
   id_disciplina INTEGER,
-  nome VARCHAR2(20),
+  nome VARCHAR2(60),
   sigla VARCHAR(10),
   area VARCHAR2(20),
   cargaHoraria INTEGER,
@@ -22,14 +20,14 @@ CREATE OR REPLACE TYPE disciplina_type AS OBJECT (
 
 CREATE OR REPLACE TYPE curso_type AS OBJECT (
   id_curso INTEGER,
-  nome VARCHAR2(20)
+  nome VARCHAR2(45)
 );
 
 CREATE OR REPLACE TYPE turma_type AS OBJECT (
-  id_disciplina REF disciplina_type NOT NULL,
-  id_professor REF professor_type NOT NULL,
-  id_curso REF curso_type NOT NULL,
-  id_sala REF sala_type NOT NULL,
+  id_disciplina REF disciplina_type ,
+  id_professor REF professor_type ,
+  id_curso REF curso_type ,
+  id_sala REF sala_type,
   periodo VARCHAR2(10),
   quantidadeAlunos INTEGER,
   mediaTurma DOUBLE PRECISION,
